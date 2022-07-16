59ed3787525ddf115bdc7110f6891a86cdb445ce
Yanir <yanir401@gmail.com>
2022-07-16 18:59:08 +0300

    step app for deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import { app } from "./app.js";
 import { PORT } from "./config/config.js";
 import "./db/mongoose.js";
 
-app.listen(PORT || 5000, (error) => {
+app.listen(process.env.PORT || PORT, (error) => {
   if (error) throw new Error("app.listen Error: " + error);
   console.log(`App listening on port ${PORT}`);
 });
