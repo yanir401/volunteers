2fe1e52aed282b145432321e6fe0838dcfb9d93e
Yanir <yanir401@gmail.com>
2022-07-16 22:04:46 +0300

    fix

--- client/src/apis/volunteerApi.js
+++ client/src/apis/volunteerApi.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const URL =
   process.env.NODE_ENV === "production" ? "/" : "http://127.0.0.1:5000";
-console.log(URL);
 
 export default axios.create({
   baseURL: URL,
