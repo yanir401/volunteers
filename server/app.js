1dfe161894d89e5ac7f69102111d61acb3591056
Yanir <yanir401@gmail.com>
2022-07-16 22:20:27 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config/config.js";
 // import "dotenv/config";
+import { fileURLToPath } from "url";
+
 import path from "path";
 import { eventRouter } from "./routes/events.route.js";
 import { userRouter } from "./routes/users.route.js";
