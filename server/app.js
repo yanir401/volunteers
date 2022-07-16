0166cb6661ea97d131a92ed360acd94146b1018f
Yanir <yanir401@gmail.com>
2022-07-16 22:26:41 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -3,13 +3,20 @@ import cors from "cors";
 import { PORT } from "./config/config.js";
 // import "dotenv/config";
 import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import path from "path";
 
 import { eventRouter } from "./routes/events.route.js";
 import { userRouter } from "./routes/users.route.js";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const publicPath = join(__dirname, "../client/build");
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+console.log("directory-name 👉️", __dirname);
+
+// 👇️ "/home/borislav/Desktop/javascript/dist/index.html"
+
+const publicPath = path.join(__dirname, "../client/build");
 
 app.use(express.static(publicPath));
 
