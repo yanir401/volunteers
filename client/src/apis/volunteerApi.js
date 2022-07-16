b4662030fc886a06f1a7aeb81b05af9a3dce7dbb
Yanir <yanir401@gmail.com>
2022-07-16 19:05:44 +0300

    fix setup for deploy

--- client/src/apis/volunteerApi.js
+++ client/src/apis/volunteerApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL = process.env.REACT_APP_API_URL;
+const URL =
+  process.env.NODE_ENV === "production" ? "/" : process.env.REACT_APP_API_URL;
 
 export default axios.create({
   baseURL: URL,
