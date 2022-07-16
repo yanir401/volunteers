4d45c720a92a35dacbb3bea8b2aa5baf588462b7
Yanir <yanir401@gmail.com>
2022-07-16 22:16:47 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -2,10 +2,21 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config/config.js";
 // import "dotenv/config";
-
+import path from "path";
 import { eventRouter } from "./routes/events.route.js";
 import { userRouter } from "./routes/users.route.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const publicPath = path.join(__dirname, "../client/build");
+
+app.use(express.static(publicPath));
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 const app = express();
 
 app.use(express.json());
