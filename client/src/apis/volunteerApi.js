afad3615cb12d21efa67cc99135290900aa87bd3
Yanir <yanir401@gmail.com>
2022-07-16 21:52:35 +0300

    deploy client

--- client/src/apis/volunteerApi.js
+++ client/src/apis/volunteerApi.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const URL =
   process.env.NODE_ENV === "production" ? "/" : "http://127.0.0.1:5000";
-console.log(URL);
 
 export default axios.create({
-  baseURL: URL,
+  baseURL: "https://lit-atoll-21726.herokuapp.com/",
 });
